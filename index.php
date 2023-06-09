6e3da6b6eac005c376bd3fd2234d52a7d6724baa
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-06-09 16:59:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Contact Form</title>
+</head>
+<body>
+  <!-- Start Form -->
+  <div class="container">
+    <form class="form">
+      <input class="form-control" type="text" name="username">
+    </form>
+  </div>
+  <!-- End Form -->
+  <script src="js/jquery-3.7.0.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>

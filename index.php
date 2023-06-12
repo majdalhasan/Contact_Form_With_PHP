5872f3f3cadb27c00aed58f18e109fea9577e71c
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-06-12 14:16:46 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,15 +10,22 @@
   <link rel="stylesheet" href="css/style.css">
   <title>Contact Form</title>
 </head>
+
 <body>
   <!-- Start Form -->
   <div class="container">
-    <form class="form">
-      <input class="form-control" type="text" name="username">
+    <h2 class="text-center">Contact Me</h2>
+    <form>
+      <input class="form-control" type="text" name="username" placeholder="Your Name">
+      <input class="form-control" type="email" name="email" placeholder="Your E-Mail">
+      <input class="form-control" type="number" name="mobile" placeholder="Your Mobile">
+      <textarea class="form-control" name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+      <input class="btn btn-success btn-block" type="submit" value="Send Message">
     </form>
   </div>
   <!-- End Form -->
   <script src="js/jquery-3.7.0.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>

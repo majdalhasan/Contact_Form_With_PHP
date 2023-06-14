91e661c439b4d1ae16b7bbc6af29d03e43604334
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-06-14 12:40:53 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+// Check If User Coming From Request
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Assign Variables
+    $user = $_POST['username'];
+    $mail = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $msg = $_POST['message'];
+
+    //Creating Array Of Errors
+
+    $formErrors = array();
+
+    if (strlen($user) <= 3 ) {
+      $formErrors[] = 'Username Must Be Larger Than 3 Characters';
+    }
+
+    if (strlen($msg) < 10 ) {
+      $formErrors[] = 'Message Can\'t Be Less Than 10 Characters';
+    }
+
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,12 +41,21 @@
 <body>
   <!-- Start Form -->
   <div class="container">
-    <h2 class="text-center">Contact Me</h2>
-    <form class="contact-form">
+    <h1 class="text-center">Contact Me</h1>
+    <div class="errors">
+      <?php
+      if(isset($formErrors)) {
+        foreach ($formErrors as $error) {
+          echo $error . '<br/>';
+        }
+      }
+      ?>
+    </div>
+    <form class="contact-form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
       <input class="form-control" type="text" name="username" placeholder="Your Name"> <i class="fa fa-user fa-fw"></i>
       <input class="form-control" type="email" name="email" placeholder="Your E-Mail"> <i class="fa-solid fa-envelope fa-fw"></i>
       <input class="form-control" type="number" name="mobile" placeholder="Your Mobile"> <i class="fa-sharp fa-solid fa-phone fa-fw"></i>
-      <textarea class="form-control" name="" id="" cols="30" rows="10" placeholder="Message"></textarea> 
+      <textarea class="form-control" name="message" id="" cols="30" rows="10" placeholder="Message"></textarea> 
       <input class="btn btn-success btn-block" type="submit" value="Send Message"><i class="fa-solid fa-paper-plane fa-fw send-icon"></i>
     </form>
   </div>
